7a73d644e9fa516f4da61509e48ff61617db27f9
dafuyi <127019390@qq.com>
2017-10-09 17:31:51 +0800

    完成约瑟夫环

--- link-list/bidirectional-ysfring/ysfring.cpp
+++ link-list/bidirectional-ysfring/ysfring.cpp
@@ -18,23 +18,31 @@ void ysfring (biLinkList &bL, int stepNum )
     ElemType *Elem;
     while (bL.len > 0 ) {
         stepNum = stepNum % bL.len;
-        if ( stepNum == 0 ) {
-            stepNum = bL.len ;
-        }
-        if ( stepNum < bL.len / 2 ) {
-//            current = bL.head;
+
+        // 如果 stepNum == 1 ，那就是当前位置的指针被删除
+        // > 1 的话就是，再往前面 stepNum - 1 个
+
+        // 找到下一个要被删除的指针位置
+        if ( stepNum < bL.len / 2 && stepNum != 0 ) {
+            // 对 stepNum 不处理
             for ( currentPos = 1; currentPos < stepNum; currentPos ++ ) {
                 current = current->next;
             }
         } else {
-//            current = bL.tail;
-            for ( currentPos = 1 ; currentPos < stepNum; currentPos ++ ) {
+            // 反向的话要对 stepNum 进行处理
+            // 如果 stepNum == 0
+            stepNum = stepNum != 0 ? bL.len - stepNum : 0;
+//            stepNum = bL.len - stepNum;
+
+            for ( currentPos = 0 ; currentPos <= stepNum; currentPos ++ ) {
                 current = current->prior;
             }
         }
+
+        // 删除这个不幸的节点
         toDelete = current;
-        current->next->prior = current->prior->next;
-        current->prior->next = current->next->prior;
+        current->next->prior = current->prior;
+        current->prior->next = current->next;
         current = current->next;
         cout << pos ++ << "th located :" << toDelete->data.locate << endl;
         stepNum = toDelete->data.passwd;
